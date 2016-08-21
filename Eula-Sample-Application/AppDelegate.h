8b26e28ff2e66cfb8f4ab90278a99c5474a9e7c4
Adam <AdamBCo@me.com>
2016-08-21 16:36:24 -0700

    Initial Commit

---
+++ Eula-Sample-Application/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Eula-Sample-Application
+//
+//  Created by Adam Cooper on 8/21/16.
+//  Copyright © 2016 Adam Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Eula-Sample-Application/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Eula-Sample-Application
+//
+//  Created by Adam Cooper on 8/21/16.
+//  Copyright © 2016 Adam Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
